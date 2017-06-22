6368d3cb3a45bc40fb72c219292a5a0ad8182f44
Kyle Heaser <kheaser@eapl.org>
2017-06-22 13:16:14 -0500

    Added all the .png files in art folder

---
+++ apps/01_05_transformations/source/app.d
@@ -0,0 +1,156 @@
+
+module app;  // 01_05_transformations
+
+import common;
+
+import std.stdio;   // writeln
+import gl3n.linalg; // mat4
+
+import derelict.util.loader;
+import derelict.util.sharedlib;
+import derelict.freetype.ft;
+import derelict.openal.al;
+import derelict.freeimage.freeimage;
+import derelict.opengl3.gl3;
+import derelict.glfw3.glfw3;
+
+
+
+void main(string[] argv)
+{
+	load_libraries();
+	
+	auto winMain = glfwCreateWindow(800, 600, "Transformations", null, null);
+	
+	glfwMakeContextCurrent(winMain); 
+	
+    // you must set the callbacks after creating the window
+
+                glfwSetKeyCallback(winMain, &onKeyEvent);
+    glfwSetFramebufferSizeCallback(winMain, &onFrameBufferResize);	
+ 
+    Shader[] shaders =
+    [
+             Shader(GL_VERTEX_SHADER, "source/vertexShader.glsl",      0),
+           Shader(GL_FRAGMENT_SHADER, "source/fragmentShader.glsl",    0)
+        //       (GL_GEOMETRY_SHADER, "source/geometryShader.glsl"     0),
+        //        (GL_COMPUTE_SHADER, "source/computeShader.glsl",     0),
+        //   (GL_TESS_CONTROL_SHADER, "source/tessControlShader.glsl", 0), 
+        //(GL_TESS_EVALUATION_SHADER, "source/tessEvalShader.glsl",    0)
+    ];
+
+    GLuint programID = createProgramFromShaders(shaders);
+
+    writeln("programID = ", programID);
+
+    GLint MaxTextureImageUnits;
+    glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS, &MaxTextureImageUnits);
+    writeln("The OpenGL 4.0 standard defines a minimum of 16 texture image units");
+    writeln("GL_MAX_TEXTURE_IMAGE_UNITS supported on this card is ", MaxTextureImageUnits); 
+ 
+    // Set up vertex data (and buffer(s)) and attribute pointers
+    GLfloat[] vertices = 
+    [
+         //  Positions         Colors      Texture Coords
+         0.5,  0.5, 0.0,   1.0, 0.0, 0.0,   1.0f, 1.0, // Top Right
+         0.5, -0.5, 0.0,   0.0, 1.0, 0.0,   1.0f, 0.0, // Bottom Right
+        -0.5, -0.5, 0.0,   0.0, 0.0, 1.0,   0.0f, 0.0, // Bottom Left
+        -0.5,  0.5, 0.0,   1.0, 1.0, 0.0,   0.0f, 1.0  // Top Left 
+    ];
+	
+    GLuint[] indices =   // Note that we start from 0
+    [
+        0, 1, 3, // First Triangle
+        1, 2, 3  // Second Triangle
+    ];
+	
+    GLuint VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.bytes, vertices.ptr, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.bytes, indices.ptr, GL_STATIC_DRAW);
+
+    // Position attribute    Data         Stride                        offset
+    //                       len
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * GLfloat.sizeof, cast(const(void)*) 0);
+    glEnableVertexAttribArray(0);
+    // Color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * GLfloat.sizeof, cast(const(void)*) (3 * GLfloat.sizeof));
+    glEnableVertexAttribArray(1);
+    // TexCoord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * GLfloat.sizeof, cast(const(void)*) (6 * GLfloat.sizeof));
+    glEnableVertexAttribArray(2);
+
+    glBindVertexArray(0); // Unbind VAO
+
+    // Load and create a texture 
+    GLuint texture1;
+    GLuint texture2;
+
+    loadTexture(texture1, "../art/container.jpg");
+
+    loadTexture(texture2, "../art/awesomeface.png");
+
+    // Bind Textures using texture units
+
+    // OpenGL should have a at least a minimum of 16 texture units for you to use 
+    // which you can activate using GL_TEXTURE0 to GL_TEXTURE15. They are deﬁned in 
+    // order so we could also get GL_TEXTURE8 via GL_TEXTURE0 + 8 for example, which 
+    // is useful when we’d have to loop over several texture units.
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glUniform1i(glGetUniformLocation(programID, "ourTexture1"), 0);
+
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glUniform1i(glGetUniformLocation(programID, "ourTexture2"), 1);
+	
+    glUseProgram(programID);
+
+    while (!glfwWindowShouldClose(winMain))    // Loop until the user closes the window
+    {
+        glfwPollEvents();  // Check if any events have been activiated (key pressed, mouse
+                           // moved etc.) and call corresponding response functions  
+        handleEvent(winMain);							   
+        // Render
+        
+        // Clear the colorbuffer
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Create transformations
+        mat4 transform = mat4.identity;
+        //mat4 transform;  // causes empty screen.  Must use = mat4.identity
+  
+        // opposite order from C++ and OpenGL and GLM
+        transform = transform.rotate(cast(GLfloat) glfwGetTime() * 1.0f, vec3(0.0f, 0.0f, -1.0f));  // negative sign
+        transform = transform.translate(vec3(0.5f, -0.5f, 0.0f));
+
+        double runningTimeInSeconds = glfwGetTime();
+        int runTimeInSecs = cast(int) runningTimeInSeconds;
+
+        // Get matrix's uniform location and set matrix
+        GLint transformLoc = glGetUniformLocation(programID, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_TRUE, transform.value_ptr);
+
+        // Draw container
+        glBindVertexArray(VAO);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, cast(const(void)*) 0);
+        glBindVertexArray(0);
+      
+        glfwSwapBuffers(winMain);   // Swap front and back buffers 
+    }
+
+    glfwTerminate();   // Clear any resources allocated by GLFW.
+	return;
+}
+
+
---
+++ windows/dll_notes.md
@@ -0,0 +1,58 @@
+
+
+## This document describes the DLL (Dynamic Link Libraries) used by the Windows. It summarizes which
+dynamic libraries are used and when and where they originally came from  
+
+### Overview
+This is an introduction to Git version control system. It teachs by doing: a step by step walkthru of all the commands listed in the required order. It is believed that theory is better understood after the user first knows the lay of the land. For Git to become really powerful/useful, it needs to paired with a remote repository.  
+
+
+### Dynamic Libraries
+    - Assimp (Assest )
+        Downloaded from fmod web site: http://assimp.sourceforge.net/ assimp 3.1.1: released June 2014
+
+        Then unzipped to create Users\user\Downloads\assimp-3.1.1-win-binaries\bin64\assimp.dll (3,716 KB)
+        and moved fmod.dll and fmodL.dll to folder beneath this document.
+
+
+    - fmod
+        Needed to create an account.  Then Downloaded from fmod web site and created: C:\Program Files (x86)\FMOD SoundSystem\FMOD Studio API Windows\api\lowlevel\lib
+
+        Renamed fmod64.dll and fmodL64.dll and changed to fmod.dll and fmodL.dll respectively.
+
+        Then moved fmod.dll and fmodL.dll to folder beneath this document.
+
+    - FreeImage
+        Went to the official FreeImage website: http://freeimage.sourceforge.net and
+
+        downloaded FreeImage 3.17.0 [WIN32/WIN64]
+
+        Get the FreeImage.dll (6,253 KB) from C:\Users\user\Downloads\FreeImage3170Win32Win64\FreeImage\Dist\x64
+        and move it to folder beneath this document.
+
+    - GLFW3
+        At the official  http://www.glfw.org/  There is a pre-built Windows pre-compiled binary for 64-bits. Then 
+        downloaded glfw-3.2.1.bin.WIN64
+        
+        Got the most current lib-vc2015 glfw3.dll (82 KB) and copied it to this folder. Note: ignored lib-vc2012 and lib-vc2013.
+    
+    - FreeType
+        
+        The libfreetype-6.dll (1,187 KB) in this folder was obtained from:
+        C:\Users\user\AppData\Roaming\dub\packages\dlangui-0.9.8\dlangui\libs\windows\x86_64  ????
+
+    - OpenAL
+        Went to the official openal website: https://www.openal.org/downloads/ and downloaded their openAL 1.1 windows 
+        installer which placed a 
+        64 bit openAL32.dll in C:\Windows\System32 and a 
+        32 bit openAL32.dll in C:\Windows\SysWOW64 
+
+        It is counterintuitive but System32 holds the 64 bit DLLs.
+    
+    - OpenGL
+        opengl32.dll seems to be installed by default in C:\Windows\System32  
+        (System32 = 64 bits; SystemWoW64 = 32 bits)
+
+        For consistency, will make a copy of the opengl32.dll and put it in the folder beneath this document.
+        
+        
