24740431b9ef0542a139e0152a6a51fa2b4a1eb2
Kyle Heaser <kheaser@eapl.org>
2017-06-28 14:40:44 -0500

    mass commit

---
+++ apps/06_03_07_post_processing/source/app.d
@@ -0,0 +1,146 @@
+module app;
+
+import common;
+import common_game;
+
+import std.math;    // cos
+import std.stdio;   // writeln
+import std.conv;    // to
+import gl3n.linalg; // vec3 mat4
+
+import derelict.util.loader;
+import derelict.util.sharedlib;
+import derelict.freetype.ft;
+import derelict.openal.al;
+import derelict.freeimage.freeimage;
+import derelict.opengl3.gl3;
+import derelict.glfw3.glfw3;
+
+bool[1024] keys;
+
+enum bool particulate = true;
+enum bool effects     = true;
+enum bool powUps      = true;
+enum bool audio       = false;
+enum bool screenText  = false;
+
+
+extern(C) static void onInternalKeyEvent(GLFWwindow* window, int key, int scancode, int action, int modifier) nothrow
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    if (key >= 0 && key < 1024)
+    {
+        if (action == GLFW_PRESS)
+            Game.keys[key] = true;
+        else if (action == GLFW_RELEASE)
+            Game.keys[key] = false;
+    }
+}
+
+/+
+
+But where is the basic unit to hold this code? Well, a code block of course, or a scope. 
+Ideally, we would like a way to group all the previous declaration into one unit, with the same parameter list:
+
+// scope?
+(Type)
+{
+    struct Tree { /* using Type here */}
+    Tree mapOnTree(Tree input, Tree delegate(Tree) f) { /* here also */ }
+    void printTree(Tree input) { /* the same */ }
+    alias TreeArray = Tree[]; // You get it
+}
+
+Since we will need to 'call' it to produce some code (a bit like you'd call a function), this code block needs a 
+name. And then, we just need to tell the compiler: 'here, this is a blueprint'. The D keyword for that is (you got it) 
+template{.d}:
+
+To instantiate a template, use the following syntax:
+
+templateName!(list, of, arguments)
+Note the exclamation point (!) before the comma-separated argument list. That's what differentiate template arguments lists from standard (funtion) argument lists. If both are present (for function templates), we will use:
+
+templateName!(template, argument, list)(runtime, agument, list)
+
+================================================================================================
+
+static if (compileTimeExpression)
+{
+     /* Code created if compileTimeExpression is evaluated to true */
+}
+
+Something really important here is a bit of compiler magic: once the code path is selected, the resulting code is instantiated in the template body, but without the curly braces. Otherwise that would create a local scope, hiding what's happening inside and would drastically limit the power of static if. So the curly braces are there only to group the statements together.
+
+
++/
+
+void main(string[] argv)
+{
+    Game breakout = new Game(800, 600);  // originally (800, 600)  // (1600, 1200) for 4K monitors
+	
+    load_libraries();
+	
+    auto winMain = glfwCreateWindow(breakout.width, breakout.height, "06_03_07_post_processing", null, null);
+		
+    glfwMakeContextCurrent(winMain); 
+
+    // you must set the callbacks after creating the window
+    glfwSetKeyCallback(winMain, &onInternalKeyEvent);	
+	
+    // GLFW Options
+    glfwSetInputMode(winMain, GLFW_CURSOR, GLFW_CURSOR_NORMAL);	
+	
+    // Define the viewport dimensions
+    glViewport(0, 0, breakout.width, breakout.height);
+
+    // Set OpenGL options
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	
+
+    // Initialize game
+    breakout.initGame();
+
+    // DeltaTime variables
+    GLfloat deltaTime = 0.0f;
+    GLfloat lastFrame = 0.0f;
+
+    // Start Game within Menu State
+    breakout.state = GameState.GAME_ACTIVE;
+	
+    // how to create and apply textures to 3D geometry data...
+    int x = 0;
+    float r = 0.001;
+		
+    //Texture2D tex = resource_manager.ResMgr.getTexture("face");	
+	
+    while (!glfwWindowShouldClose(winMain))    // Loop until the user closes the window
+    {     
+        // Calculate delta time
+        GLfloat currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        glfwPollEvents();
+
+        deltaTime = 0.001f;
+        // Manage user input
+        breakout.processInput(deltaTime);
+
+        // Update Game state
+		
+        breakout.update_04(deltaTime);
+
+        // Render
+        //glClearColor(0.1f, 0.3f, 0.4f, 1.0f);  // originally
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+		breakout.renderGameWithParticles();
+		
+        glfwSwapBuffers(winMain);
+    }
+    return;
+}
+
---
+++ apps/06_03_10_render_text/source/app.d
@@ -0,0 +1,146 @@
+module app;
+
+import common;
+import common_game;
+
+import std.math;    // cos
+import std.stdio;   // writeln
+import std.conv;    // to
+import gl3n.linalg; // vec3 mat4
+
+import derelict.util.loader;
+import derelict.util.sharedlib;
+import derelict.freetype.ft;
+import derelict.openal.al;
+import derelict.freeimage.freeimage;
+import derelict.opengl3.gl3;
+import derelict.glfw3.glfw3;
+import derelict.fmod.fmod;
+//import derelict.fmod.common;
+
+bool[1024] keys;
+
+enum bool particulate = true;
+enum bool effects     = true;
+enum bool powUps      = true;
+enum bool audio       = true;
+enum bool screenText  = true;
+
+
+extern(C) static void onInternalKeyEvent(GLFWwindow* window, int key, int scancode, int action, int modifier) nothrow
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    if (key >= 0 && key < 1024)
+    {
+        if (action == GLFW_PRESS)
+            Game.keys[key] = true;
+        else if (action == GLFW_RELEASE)
+            Game.keys[key] = false;
+    }
+}
+
+
+
+
+SoundSystem soundSys;  // Structure containing audio functionality
+
+TextRenderer textRend;
+
+TextRenderingSystem textRenderSys;	// works
+
+
+void main(string[] argv)
+{
+    Game breakout = new Game(800, 600);  // originally (800, 600)  // (1600, 1200) for 4K monitors
+	
+    load_libraries();
+	
+    initSoundSystem(soundSys);
+
+    playSound(FMOD_LOOP_NORMAL, soundSys.system, "../audio/breakout.mp3");
+	
+    printDrivers();	
+
+    initSound(soundSys, FMOD_LOOP_NORMAL, "../audio/breakout.mp3");  // offset 0	
+    initSound(soundSys, FMOD_LOOP_OFF,    "../audio/bleep.mp3");     // offset 1
+    initSound(soundSys, FMOD_LOOP_OFF,    "../audio/solid.wav");     // offset 2	
+    initSound(soundSys, FMOD_LOOP_OFF,    "../audio/powerup.wav");   // offset 3	
+    initSound(soundSys, FMOD_LOOP_OFF,    "../audio/bleep.wav");     // offset 4	
+	
+    playSound(soundSys, 0 );		
+	
+	
+    auto winMain = glfwCreateWindow(breakout.width, breakout.height, "06_03_10_render_text", null, null);
+		
+    glfwMakeContextCurrent(winMain); 
+
+    // you must set the callbacks after creating the window
+    glfwSetKeyCallback(winMain, &onInternalKeyEvent);	
+	
+    // GLFW Options
+    glfwSetInputMode(winMain, GLFW_CURSOR, GLFW_CURSOR_NORMAL);	
+	
+    // Define the viewport dimensions
+    glViewport(0, 0, breakout.width, breakout.height);
+
+    // Set OpenGL options
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	
+
+    // Initialize game
+    breakout.initGame();
+
+    // DeltaTime variables
+    GLfloat deltaTime = 0.0f;
+    GLfloat lastFrame = 0.0f;
+
+    // Start Game within Menu State
+    breakout.state = GameState.GAME_ACTIVE;
+	
+    // how to create and apply textures to 3D geometry data...
+    int x = 0;
+    float r = 0.001;
+		
+    //Texture2D tex = resource_manager.ResMgr.getTexture("face");	
+	
+ 
+	
+    initTextRenderingSystem(textRenderSys);		
+	
+    while (!glfwWindowShouldClose(winMain))    // Loop until the user closes the window
+    {     
+        // Calculate delta time
+        GLfloat currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        glfwPollEvents();
+
+        deltaTime = 0.001f;
+        // Manage user input
+        breakout.processInput(deltaTime);
+
+        // Update Game state
+		
+        breakout.update_04(deltaTime);
+
+        // Render
+        //glClearColor(0.1f, 0.3f, 0.4f, 1.0f);  // originally
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+		breakout.renderGameWithParticles();
+
+        //textRend.renderText("KYLE HEASER textRend.renderText", 50.0f, 50.0f, 1.0);
+		
+	    //renderText(textRenderSys.font, textRenderSys.VAO, textRenderSys.VBO, textRenderSys.progID, 
+        //           "Courier Bold Text", 25.0f, 75.0f, 2.5f, vec3(1.0, 1.0f, 1.0f));	
+		
+        glfwSwapBuffers(winMain);
+    }
+    return;
+}
+
+
